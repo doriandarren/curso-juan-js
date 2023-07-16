3d27e897dd94795d03e6a20272850cd7d3f00f1f
Dorian Gonzalez <doriandarren1@gmail.com>
2023-07-16 19:24:59 +0200

    first commit

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,12 @@
+// //pregunta al usaurio su nombre
+// const nombre = prompt('Cual es tu nombre?');
+
+// // Toma el nombre y lo muestra en pantalla
+// document.querySelector('.contenido').innerHTML = `${nombre} Está aprendiendo Javascript Moderno`;
+
+// // Asigna el valor hacia la variable producto
+// const producto = 'Monitor 24 pulgadas';
+
+
+console.log('hola');
+console.log('Mundo');
