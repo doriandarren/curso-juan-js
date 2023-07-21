2dd0270af9cb9403edfbb28de2ae02bd6af937d7
Dorian Gonzalez <doriandarren1@gmail.com>
2023-07-21 19:44:56 +0200

    first array

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,14 @@
+
+const numeros = [10, 20, 30];
+
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numeros);
+console.log(meses);
+
+
+// arreglo que contiene datos de todo tipo;
+
+const deTodo = ['Hola', 10, true, 'si', null, { nombre: 'Juam', trabajo: 'Programador'}, [1,2,3]];
+
+console.log(deTodo);
