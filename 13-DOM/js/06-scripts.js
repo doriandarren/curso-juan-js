7236cc7250c3a4e8880a51e2bd37d4379de64e66
Dorian Gonzalez <doriandarren1@gmail.com>
2023-09-12 20:56:40 +0200

    add capter 6

---
+++ 13-DOM/js/06-scripts.js
@@ -0,0 +1,22 @@
+
+
+const encabezado = document.querySelector('.contenido-hero h1').textContent;
+
+console.log(encabezado);
+
+
+// console.log(encabezado.innerText); // si en CSS visibility no lo encuentra
+// console.log(encabezado.textContent);
+// console.log(encabezado.innerHTML);
+
+
+// const nuevoHeading = 'Nuevo heading';
+// document.querySelector('.contenido-hero h1').textContent = nuevoHeading;
+
+
+
+const imagen = document.querySelector('.card img');
+console.log(imagen);
+
+imagen.src = 'img/hacer2.jpg';
+
---
+++ 13-DOM/js/08-scripts.js
@@ -0,0 +1,14 @@
+const navegacion = document.querySelector('.navegacion');
+
+// console.log(navegacion);
+// console.log(navegacion.childNodes); // los espacios en blancos son considerados elementos
+// console.log(navegacion.children); // elementos en HTML
+
+
+// console.log(navegacion.children[1].nodeName);
+// console.log(navegacion.children[1].nodeType);
+
+
+const card = document.querySelector('.card');
+
+console.log(card.children);
