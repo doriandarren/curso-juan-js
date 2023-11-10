47740cea8af55dbc27196491098bc4ee69d7c775
Dorian Gonzalez <doriandarren1@gmail.com>
2023-11-10 22:10:44 +0100

    add carito

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -5,6 +5,8 @@ const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 
+let articulosCarrito = [];
+
 
 cargarEventListeners();
 
@@ -39,5 +41,85 @@ function leerDatosCurso(curso){
         cantidad: 1
     }
 
-    console.log(infoCurso);
+
+    //revisa si un elemento ya existe en el carrito
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id );
+
+    if(existe){
+        //Actualizamos la cantidad
+
+        const cursos = articulosCarrito.map( curso => {
+            if(curso.id === infoCurso.id){
+                curso.cantidad++;
+                return curso;
+            }else{
+                return curso;
+            }
+        } );
+
+        articulosCarrito = [...cursos];
+
+    }else{
+        //Agregamos el curso al carrito
+
+        //agregar ekementos al arreglo de carrito
+        articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+
+
+
+    
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+
+}
+
+
+
+//Muestar el carrito de compras
+function carritoHTML() {
+
+    //Limpiar Html
+    limpiarHTML();
+
+
+    articulosCarrito.forEach((curso) => {
+        const { imagen, titulo, precio, cantidad, id } = curso;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${imagen}" width="100">
+            </td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${id}" > X </a>
+            </td>
+        `;
+
+        //Agrega el Html del carrito en el tbody
+
+        contenedorCarrito.appendChild(row);
+
+    });
+
+}
+
+
+//Eliminar los cursos del tbody
+function limpiarHTML() {
+    // Forma lenta
+    //contenedorCarrito.innerHTML = '';
+
+
+    // Forma más rápida
+    while(contenedorCarrito.firstChild){
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+
+
 }
