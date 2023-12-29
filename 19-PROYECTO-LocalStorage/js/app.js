7c27601618b6cebeb375183650052853cdf72ce0
Dorian Gonzalez <doriandarren1@gmail.com>
2023-12-29 22:34:28 +0100

    finish project 19

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -9,7 +9,7 @@
 
 const formulario = document.querySelector('#formulario');
 const listaTweets = document.querySelector('#lista-tweets');
-const tweets = [];
+let tweets = [];
 
 
 
@@ -31,7 +31,16 @@ const tweets = [];
  eventListeners();
 
  function eventListeners(){
+    //Cuando el usuario agrega un nuevo tweet
     formulario.addEventListener('submit', agregarTweet);
+
+    //Cuando el documento este listo
+    document.addEventListener('DOMContentLoaded', () => {
+        tweets = JSON.parse(localStorage.getItem('tweets')) || [];
+        console.log(tweets);
+
+        crearHTML();
+    });
  }
 
 
@@ -62,9 +71,19 @@ const tweets = [];
         return;
     }
 
+    const tweetObj = {
+        id: Date.now(),
+        tweet
+    }
 
+    //Añadir al Arrglo de tweets
+    tweets = [...tweets, tweetObj];
 
-    console.log(tweet);
+    //una vez agregado creamos el HTML
+    crearHTML();
+
+    //Reiniciar el form
+    formulario.reset();
 
 
 
@@ -88,4 +107,63 @@ function mostrarError(error){
         mensajeError.remove();
     }, 3000);
 
+}
+
+
+//Muestra un listado de tweets
+function crearHTML() {
+
+    limpiarHTML();
+
+    if(tweets.length > 0){
+        tweets.forEach(tweet => {
+
+            //Agregar un boton de eliminar 
+            const btnEliminar = document.createElement('a');
+            btnEliminar.classList.add('borrar-tweet');
+            btnEliminar.innerText = 'X';
+
+            //Añadir la función 
+            btnEliminar.onclick = () => {
+                borrarTweet(tweet.id);
+            }
+
+
+            //Crear HTML
+            const li = document.createElement('li');
+            li.innerText = tweet.tweet;
+
+            //Asignar el boton
+            li.appendChild(btnEliminar);
+
+            //Agregar en el html
+            listaTweets.appendChild(li);
+
+        });
+    }
+
+    sincronizarStorage();
+
+}
+
+
+//Agregar al localstorage
+function sincronizarStorage(){
+    localStorage.setItem('tweets', JSON.stringify(tweets));
+}
+
+
+//Eliminar un tweet
+function borrarTweet(id){
+    tweets = tweets.filter(tweets => tweets.id !== id);
+    crearHTML();
+}
+
+
+
+// Limpiar el HTML
+function limpiarHTML(){
+    while(listaTweets.firstChild){
+        listaTweets.removeChild(listaTweets.firstChild);
+    }
 }
